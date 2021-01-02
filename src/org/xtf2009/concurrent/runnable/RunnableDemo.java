23c68566d35089888fd3543876482bfb62010105
Xia Tingfei <xiatf@deepinno.com>
2021-01-03 00:23:36 +0800

    添加了runnable示例

--- src/org/xtf2009/concurrent/runnable/RunnableDemo.java
+++ src/org/xtf2009/concurrent/runnable/RunnableDemo.java
@@ -1,4 +1,16 @@
 package org.xtf2009.concurrent.runnable;
 
-public class RunnableDemo {
+public class RunnableDemo implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("I am running in Thread:" + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start main in Thread:"+ Thread.currentThread().getName());
+        Thread t = new Thread(new RunnableDemo());//创建一个Thread类，传入RunnableDemo的实例
+        t.start();
+        System.out.println("End main in Thread:"+ Thread.currentThread().getName());
+    }
 }
